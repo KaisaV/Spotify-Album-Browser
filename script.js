997ca9cd687755ab0babf45cf5fcd6c802ded53a
alien O_O <kaisa.voutilainen@aalto.fi>
2017-02-19 22:59:15 +0200

    Clicking an album cover opens a modal box that has the high resolution image

--- script.js
+++ script.js
@@ -2,14 +2,15 @@ var xmlhttp = new XMLHttpRequest();
 var url = "https://api.spotify.com/v1/search";
 var albums;
 var coverSize = '150';
+var n ;
 
 function listAlbums(i){
-    
+        n += 1;
         var imgSrc = i.images[1].url;
         var artist = i.artists[0].name;
         var album = i.name;
         album = album.replace(/ /g, "&nbsp;");
-        document.getElementById("covers").innerHTML += "<div class='albumCover' title='Title: "+album+"\nArtist: "+artist.replace(/ /g, "&nbsp;")+ "'><img src="+imgSrc+" width="+coverSize +"height="+coverSize+"><p class='albumInfo'>Title: "+i.name+"<br>Artist: " +artist+"</p></div>";
+        document.getElementById("covers").innerHTML += "<div class='albumCover' onclick='showHRPic("+n+")' title='Title: "+album+"\nArtist: "+artist.replace(/ /g, "&nbsp;")+ "'><img src="+imgSrc+" width="+coverSize +"height="+coverSize+"><p class='albumInfo'>Title: "+i.name+"<br>Artist: " +artist+"</p></div>";
     
 }
 
@@ -17,8 +18,10 @@ xmlhttp.onreadystatechange = function(){
     if(this.readyState == 4 && this.status == 200){
         var tmp = JSON.parse(this.responseText);
         albums = tmp.albums.items;
+        n = 0;
         document.getElementById("covers").innerHTML = "";
         albums.forEach(listAlbums);
+        document.getElementById("covers").innerHTML += "<img id='overLayPic' >"
     }
 };
 
@@ -27,5 +30,18 @@ function searchAlbums(){
     document.getElementById('key').value = "";
     xmlhttp.open("GET", url, true);
     xmlhttp.send();
+}
+
+function showHRPic(x){
+    //window.alert(i.images[0].url);
+    //document.getElementById("overLayPic").innerHTML = "<img id='overLayPic' src="+albums[x-1].images[0].url+">";
+    document.getElementById("modalBox").style.display = "block";
+    //window.alert(albums[x-1].images[0].url);
+    //document.getElementById("HRPic").innerHTML = "<img id='HRPic' src="+albums[x-1].images[0].url+">"
+    document.getElementById("HRPic").src = albums[x-1].images[0].url;
+}
+
+function closeBox(){
+    document.getElementById("modalBox").style.display = "none";
 }
 
